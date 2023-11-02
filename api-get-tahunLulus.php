82244919e034417fa770a3e98b76435660cdc188
rezalfara <reza.alfara07@gmail.com>
2023-11-03 02:06:11 +0700

    commit

--- api-get-tahunLulus.php
+++ api-get-tahunLulus.php
@@ -2,11 +2,7 @@
 include 'koneksi.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    $query_read = "SELECT a.*, j.nama_jurusan, t.tahun_lulus
-    FROM alumni a
-    JOIN jurusan j ON a.id_jurusan = j.id_jurusan
-    JOIN tahun_lulus t ON a.id_tahun_lulus = t.id_tahun_lulus";
-    
+    $query_read = "SELECT * FROM tahun_lulus";
     $result = mysqli_query($koneksi, $query_read);
 
     if (mysqli_num_rows($result) > 0) {
@@ -16,7 +12,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
         }
         echo json_encode($data);
     } else {
-        echo json_encode(array("message" => "Data alumni tidak ditemukan"));
+        echo json_encode(array("message" => "Data tahun lulus tidak ditemukan"));
     }
 } else {
     echo json_encode(array("message" => "Metode permintaan tidak valid"));
